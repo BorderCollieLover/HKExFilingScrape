395eb3aa6b5ff6bbb2dd0834a807a25da60b65e3
Min Tang <56982568+BorderCollieLover@users.noreply.github.com>
2021-06-12 18:57:24 +0800

    Update b2b_phillip_new.R

--- b2b_phillip_new.R
+++ b2b_phillip_new.R
@@ -1,18 +1,15 @@
 ##for phillip trade
-
 #daily setting
 #today; yesterday; day before yesterday; day after tommorrow
+
 library("stringr")
 library("readr")
 library("pdftools")
 library('bizdays')
 
-
-
 #set the statement date and format it to contract notes naming convention
 #use the existing code although it is probably better to consider using business days instead of using a fixed offset
 #in addition, it might be better to consider scanning for downloaded/existing contract notes 
-
 statement_dt <- function() { 
 	if(weekdays(today)=="Monday"){
 		fnm_y<-format(today-3,format="%Y%m%d");fnm_y
@@ -24,45 +21,48 @@ statement_dt <- function() {
 	last_t
 }
 
-
 #read the contents from the contract note PDF file
-#path<-paste('Z:/DayEnd Report/Phillip Daily Report/Contract note for Margin Account - 2065959',sep="")
-contrat_note_filename <- function(dt) {
+#input : dt format to the specific contract note filename convention
+#output: the contents of the contract note as an array of strings  
+contract_note_contents <- function(dt) {
+	#path<-paste('Z:/DayEnd Report/Phillip Daily Report/Contract note for Margin Account - 2065959',sep="")
 	path<-paste('Z:/',sep="")
-	file<-paste('Contract note for Margin Account - 2065959-',last_t,'.pdf',sep='')
+	file<-paste('Contract note for Margin Account - 2065959-', dt,'.pdf',sep='')
+	php_text<-pdf_text(pdf=paste(path,'/',file,sep=''))%>% readr::read_lines()
 }
-php_text<-pdf_text(pdf=paste(path,'/',file,sep=''))%>% readr::read_lines()
 
 
 #data cleaning
-title_start<-which(str_detect(php_text,'STATEMENT'))
-title_end<-which(str_detect(php_text,'STATEMENT'))+5
-title_list<-NULL
-for (i in 1:length(title_start)){
-	title<-seq(title_start[i],title_end[i],1)
-	title_list<-c(title_list,title)	
+cleaned_contract_note_contents <- function(php_text) {
+	
+	title_start<-which(str_detect(php_text,'STATEMENT'))
+	title_end<-which(str_detect(php_text,'STATEMENT'))+5
+	title_list<-NULL
+	for (i in 1:length(title_start)){
+		title<-seq(title_start[i],title_end[i],1)
+		title_list<-c(title_list,title)	
 	}
-title_list
+	title_list
 
-note_start<-which(str_detect(php_text,'Important Notes'))
-note_end<-which(str_detect(php_text,'Important Notes'))+12
-note_list<-NULL
-for (i in 1:length(note_start)){
-	title<-seq(note_start[i],note_end[i],1)
-	note_list<-c(note_list,title)	
+	note_start<-which(str_detect(php_text,'Important Notes'))
+	note_end<-which(str_detect(php_text,'Important Notes'))+12
+	note_list<-NULL
+	for (i in 1:length(note_start)){
+		title<-seq(note_start[i],note_end[i],1)
+		note_list<-c(note_list,title)	
 	}
-note_list
+	note_list
 
-page_start<-which(str_detect(php_text,'Page'))
-page_end<-which(str_detect(php_text,'Page'))+1
-page_list<-NULL
-for (i in 1:length(page_start)){
-	title<-seq(page_start[i],page_end[i],1)
-	page_list<-c(page_list,title)	
+	page_start<-which(str_detect(php_text,'Page'))
+	page_end<-which(str_detect(php_text,'Page'))+1
+	page_list<-NULL
+	for (i in 1:length(page_start)){
+		title<-seq(page_start[i],page_end[i],1)
+		page_list<-c(page_list,title)	
 	}
-page_list
+	page_list
 
-php_text<-php_text[-c(title_list,note_list,page_list)]
+	php_text<-php_text[-c(title_list,note_list,page_list)]
 
 #remove contracts; trans;cd;date etc
 php_text<-php_text[-c(which(str_detect(php_text,'CONTRACT|TRANS|CD|DATE|Comm|TOTAL|SETTLEMENT')))]
